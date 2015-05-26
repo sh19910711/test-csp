6988268526010b26ce5e8a83d3edafe090c09bb4
Hiroyuki Sano <sh19910711@gmail.com>
2015-05-26 18:31:19 +0900

    app: add App class

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "json"
+
+class App < Padrino::Application
+
+  register Padrino::Helpers
+
+  RESULT_OK = {
+    :result => "OK",
+  }.to_json
+
+  get "/some/api" do
+    RESULT_OK
+  end
+
+  get "/api/with/custom/header" do
+    headers "X-Hello" => "WORLD"
+    RESULT_OK
+  end
+
+end
